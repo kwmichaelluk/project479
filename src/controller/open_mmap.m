2beb27b4b43ad15c21162b677cb894b42785ce26
= <=>
2013-09-28 19:23:34 -0700

    Added main.m, with a fake physics engine

--- src/controller/open_mmap.m
+++ src/controller/open_mmap.m
@@ -1,5 +1,5 @@
-function [mmap] = open_mmap()
-    mmap_filename = 'data/data_pos';
+function [mmap] = open_mmap(mmap_filename)
+    %mmap_filename = ['data',filesep,'data_pos'];
     
     mmap = memmapfile(mmap_filename,'Format','double');
     mmap.Writable = true;
